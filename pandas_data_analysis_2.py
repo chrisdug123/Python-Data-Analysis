81d5ed72c0d51cd6322588f6339f55a960ea216c
Christopher Dugdale <chris.dugdale3@gmail.com>
2024-08-11 22:47:28 +1000

    Introduced some simple splicing and series / df manipulation

--- pandas_data_analysis_2.py
+++ pandas_data_analysis_2.py
@@ -3,15 +3,55 @@ import pandas as pd
 
 
 # Series
-data_series = np.random.normal(0,1,100)
-p_series = pd.Series(data_series)
+#data_series = np.random.normal(0,1,100)
+#p_series = pd.Series(data_series)
 
 #print(p_series.head(5))
 #print(p_series.tail(5))
 
-data_series_2 = np.random.normal(0,5,100)
-frame={'column1':data_series,
-    'column2': data_series_2}
+#data_series_2 = np.random.normal(0,5,100)
+#frame={'column1':data_series,
+#    'column2': data_series_2}
 
-df=pd.DataFrame(frame)
+#df=pd.DataFrame(frame)
+
+#students_ids = [n for n in range(200,220)]
+##math_scores = np.random.normal(70,10,20)
+#english_scores = np.random.normal(65,8,20)
+#history_scores = np.random.normal(80,7,20)
+
+
+
+#df=pd.DataFrame({
+#    'Student ID': students_ids,
+#    'Math Scores': math_scores,
+#    'English Scores': english_scores,
+#    'History Scores': history_scores
+#})
+
+#print(df)
+#print("Math Scores: ", df['Math Scores'])
+#print("Sorted by Maths: ", df.sort_values(by='Math Scores'))
+#print("Rows 5 - 10: ", df.loc[5:10])
+#print("Rows 5-10, Only Math and English: ", df.loc[5:10,['Math Scores','English Scores']])
+#print(".loc version: ", df.iloc[5:10,1:3])
+
+#print(df[df['Math Scores']>75])
+
+#print(df[(df['Math Scores']>75) & (df['English Scores']<60)])
+
+#df['Total'] = df['Math Scores'] + df['English Scores'] + df['History Scores']
+
+#df.drop(columns='Total', inplace=True)
+#print(df)
+
+data = {
+    'A': [1,2, np.nan, 4, 5],
+    'B': [np.nan, 2,3,4,5],
+    'C':[1,2,3, np.nan, np.nan]
+}
+
+df =pd.DataFrame(data)
+print(df)
+
 
